121f4a1de06c9c0b1f208823ef9ff3042c82bc16
Ben Rudinski <105463867+Tuvalu23@users.noreply.github.com>
2024-09-20 11:27:21 -0400

    Add files via upload

---
+++ app.py
@@ -0,0 +1,35 @@
+# your heading here
+
+'''
+DISCO:
+<note any discoveries you made here... no matter how small!>
+
+QCC:
+0. 
+1. 
+2. 
+3. 
+4. 
+5. 
+ ...
+
+INVESTIGATIVE APPROACH:
+<Your concise summary of how
+ you and your team set about
+ "illuminating the cave of ignorance" here...>
+'''
+
+
+from flask import Flask
+
+app = Flask(__name__)                    # Q0: Where have you seen similar syntax in other langs?
+
+@app.route("/")                          # Q1: What points of reference do you have for meaning of '/'?
+def hello_world():
+    print(__name__)                      # Q2: Where will this print to? Q3: What will it print?
+    return "No hablo queso!"             # Q4: Will this appear anywhere? How u know?
+
+app.run()                                # Q5: Where have you seen similar constructs in other languages?
+
+
+
