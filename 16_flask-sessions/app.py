077c10b2435e1f70723eb0c64e0a77b9d6cd0ba2
Tuvalu23 <brudinski50@stuy.edu>
2024-10-09 16:44:39 -0400

    16 complete

--- 16_flask-sessions/app.py
+++ 16_flask-sessions/app.py
@@ -6,7 +6,7 @@ K16: Take and Keep
 2024-10-09
 time spent: 1 hr
 '''
-from flask import Flask, render_template, request, url_for, session
+from flask import Flask, render_template, request, redirect, url_for, session
 
 app = Flask(__name__)
 app.secret_key = 'Topher1234' # key used for session management
@@ -29,6 +29,29 @@ def login():
     
 @app.route('/welcome')
 def welcome():
+    if 'username' in session:
+        username = session['username']
+        greeting = f"Hello, {username}! Welcome to this beautiful Flask App!"
+        method_used = request.method
+        
+        explanation = """
+        Now you're logged in! You will remain logged in until you log out.
+        This Flask app uses sessions, which are managed via cookies. Your session data is stored on the server,
+        and a session cookie is sent to your browser.
+        """
+        
+        return render_template('response.html', username=username, method=method_used, 
+                               greeting=greeting, explanation=explanation, 
+                               team_name=team_name, roster=roster)
+    else:
+        return redirect(url_for('index'))
 
+@app.route('/logout')
+def logout():
+    username = session.pop('username', None) # remove user from session
+    print(f"DEBUG: User {username} logged out!")
+    return render_template('logout.html', team_name=team_name, roster=roster)
+    
+        
 if __name__ == '__main__':
     app.run(debug=True)
