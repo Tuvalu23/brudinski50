19941c428d9b68912845d085221873e4b7cf9ddd
Tuvalu23 <brudinski50@stuy.edu>
2024-11-20 18:52:19 -0500

    complete 23

--- 23_rest/app.py
+++ 23_rest/app.py
@@ -2,20 +2,44 @@
 Ben Rudinski
 TopherAPI
 SoftDev
-K23: Rest -- Use an API key to display information from the (NASA) API
+K23: RESTful Journey Skyward -- Use an API key to display information from the NASA API
 2024-11-20
 time spent: 1 hr
 '''
 from flask import Flask, render_template, request
+import urllib.request # request api
+import json # send api
 
 app = Flask(__name__)
 
 team_name = "TopherAPI"
 roster = ["Ben Rudinski + Tiffany Yang"]
 
+# read nasa api key from file
+with open("key_nasa.txt", "r") as topher:
+    api_key = topher.read().strip()
+
 @app.route('/')
 def main():
-    return render_template('main.html', team_name=team_name, roster=roster)
+    
+    # api endpoint for picture of the day
+    nasa_url = f"https://api.nasa.gov/planetary/apod?api_key={api_key}"
+    
+    # parse JSON response and make request
+    try:
+        response = urllib.request.urlopen(nasa_url)
+        data = json.loads(response.read())
+        print(json.dumps(data, indent=4)) # debug to print API response
+    except Exception as e:
+        data = {"error": str(e)}
+        print(f"There was an error fetching your data from NASA API: {e}")
+    
+    #extract the data
+    image_url = data.get('url')
+    explanation = data.get('explanation')
+    title = data.get('title')
+    
+    return render_template('main.html', team_name=team_name, roster=roster, image_url=image_url, explanation=explanation, title=title)
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ 23_rest/readme.md
@@ -0,0 +1,28 @@
+# K23: A RESTful Journey Skyward
+
+## TNPG: TopherAPI
+
+### Roster:
+- Ben Rudinski
+- Tiffany Yang
+
+---
+
+## DISCO 
+- I learned that the APOD can be a video, and potentially in the future we can handle those as well
+- I discovered how to use Jinja2 templating to  render content based on variables passed from Flask.
+
+## QCC
+- How can we make theimages more responsive to different screen sizes and add different + more complex styles to them?
+- What are the best practices for displaying error messages to users in a production environment?
+
+---
+
+## Instructions
+
+1. **API Key**: Ensure you have your NASA API key saved in `key_nasa.txt`. This file should contain only the API key and should not be tracked by git.
+
+2. **Install Flask**: Make sure Flask is installed in your Python environment.
+
+   ```bash
+   pip install flask
